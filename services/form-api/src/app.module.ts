d7140f9e1ada70a6a99bba1f5cef0fee2301ddf4
nabixka <nabixka05@gmail.com>
2026-08-05 08:52:25 +0700

    add category

--- services/form-api/src/app.module.ts
+++ services/form-api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { FormModule } from './form/form.module';
 import { SoalModule } from './soal/soal.module';
 import { QrCodeModule } from './qr.code/qr.code.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [FormModule, SoalModule, QrCodeModule]
+  imports: [FormModule, SoalModule, QrCodeModule, CategoryModule]
 })
 export class AppModule {}
